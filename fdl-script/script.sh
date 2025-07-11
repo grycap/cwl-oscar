dd127f41821bdfd658fc0e344595bbcb73951cd4
esparig <5576714+esparig@users.noreply.github.com>
2025-07-11 16:59:39 +0200

    Added example to create OSCAR Service that executes a command received through the event

---
+++ fdl-script/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Running run-script-event: File available in $INPUT_FILE_PATH"
+
+FILE_NAME=$(basename "$INPUT_FILE_PATH")
+OUTPUT_FILE="$TMP_OUTPUT_DIR/$FILE_NAME.output"
+ERROR_FILE="$TMP_OUTPUT_DIR/$FILE_NAME.error"
+
+# Execute the contents of the input file
+`cat "$INPUT_FILE_PATH"` 1> "$MOUNT_PATH/$FILE_NAME.output" 2> "$ERROR_FILE"; echo $? > "$OUTPUT_FILE"
+
+echo "Script completed."
