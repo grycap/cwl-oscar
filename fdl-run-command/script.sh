f5fd9052f3ebac1f407dcf198e8d281452f6f66c
esparig <5576714+esparig@users.noreply.github.com>
2025-07-24 10:25:15 +0200

    Added example to create OSCAR Service that executes a bash script received as file in the input bucket

--- fdl-run-command/script.sh
+++ fdl-run-command/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Running run-script-event: File available in $INPUT_FILE_PATH"
+echo "Running run-command: File available in $INPUT_FILE_PATH"
 
 # Sleeping for 10 seconds
-echo "SCRIPT: Sleeping for 10 seconds"
+echo "[script.sh] Sleeping for 10 seconds"
 sleep 10
 
 FILE_NAME=$(basename "$INPUT_FILE_PATH")
@@ -11,11 +11,11 @@ OUTPUT_FILE="$TMP_OUTPUT_DIR/$FILE_NAME.output"
 ERROR_FILE="$TMP_OUTPUT_DIR/$FILE_NAME.error"
 
 # Check if the mount path is available
-echo "SCRIPT: Checking if the mount path is available"
+echo "[script.sh] Checking if the mount path is available"
 ls -lah /mnt
 
 # Check if the output file is available
-echo "SCRIPT: Checking if the $MOUNT_PATH file is available"
+echo "[script.sh] Checking if the $MOUNT_PATH file is available"
 ls -lah "$MOUNT_PATH"
 
 # Execute the contents of the input file
