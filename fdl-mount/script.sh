45ee8fee6c176a4b3e01c2b09e36a0f0db0c40b3
Robert Nica <rnica@biobam.com>
2025-08-30 02:27:11 +0200

    clean up

--- fdl-mount/script.sh
+++ fdl-mount/script.sh
@@ -4,6 +4,7 @@ echo "Running copy-input-mounted-vol: File available in $INPUT_FILE_PATH"
 FILE_NAME=$(basename "$INPUT_FILE_PATH")
 OUTPUT_FILE="$TMP_OUTPUT_DIR/$FILE_NAME.output"
 echo "Copying input file $INPUT_FILE_PATH to mounted volume $MOUNT_PATH/$FILE_NAME"
+sleep 5
 cp "$INPUT_FILE_PATH" "$MOUNT_PATH/$FILE_NAME"
 echo "List contents of $MOUNT_PATH:"
 ls -l "$MOUNT_PATH" 
